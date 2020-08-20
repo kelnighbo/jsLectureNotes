1e302769b0b460d04ab5a512604cffb442b12603
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2020-08-20 16:01:50 -0400

    first commit

---
+++ week1/01.02.js
@@ -0,0 +1,14 @@
+/**************************
+FIZZ BUZZ (Conditionals Only)
+**************************/
+/*
+    Challenge:
+    Create a variable named FB that takes numbers
+    Write an ELSE/IF conditional that: 
+    Prints out "Fizz" if the number is divisible by 3
+    Prints out "Buzz" if the number is divisible by 5
+    Prints out "Fizz Buzz" if the number is divisible by BOTH 3 and 5
+    Convert this to SWITCH STATEMENT and then a TERNARY
+*/
+
+let FB = 1
---
+++ week2/02.02.js
@@ -0,0 +1,10 @@
+// 08-19- init
+
+
+// 0. git
+
+
+ /*
+
+ This is a change!
+*/
